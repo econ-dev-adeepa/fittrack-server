1a4d24cae0dfd74375981556a89281289e39434c
Adeepa Gunathilake <adeepa.g@econsulate.net>
2026-03-30 17:31:19 +0530

    feat: add UsersController to manage user-related endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    controllers: [],
+    controllers: [UsersController],
     providers: [UsersService],
     exports: [UsersService],
 })
